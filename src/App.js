804f2962b4d1b1c2811153714c8198634b457fc4
BrendaGilisho <b.gilisho@alustudent.com>
2021-11-19 13:36:21 +0200

    feature

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Test from './components/tes'
+
+function App() {
+  return (
+    <div>
+
+      <h1>welcome</h1>
+      < Test />
+      
+    </div>
+  );
+}
+
+export default App;
